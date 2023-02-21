bb13db57fedc43248570facad24c16790b1cc650
Eldar <9eldik7@gmail.com>
2023-02-21 12:42:45 +0300

    main logic

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"log"
 	"main/open"
+	"main/search"
+	"main/spam"
 )
 
 func main() {
-	ctx := open.Open()
+	ctx, cancel := open.Open()
+	defer cancel()
 
 	ctx, links := search.Search(ctx)
+	log.Println("Searched ", len(links), " link(s)")
 
-	ctx = spam.Spam(ctx, links)
+	ctx, successfulCount := spam.Spam(ctx, links)
+	log.Println("Response ", successfulCount)
 }
